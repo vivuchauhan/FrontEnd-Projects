737654569c751752b63fd373efa2746dcc13f44e
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2023-01-24 13:05:25 +0530

    Learning React

---
+++ src/Components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+import LeftNavbar from './LeftNavbar'
+const Container = () => {
+  return (
+      <div className='hero'>
+          <LeftNavbar/>
+          
+      </div>
+  )
+}
+
+export default Container

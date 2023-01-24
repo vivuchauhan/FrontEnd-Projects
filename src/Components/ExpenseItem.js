8976c50e29f75b2bcaee281e44c20cb3ae7b0c88
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2023-01-25 00:31:32 +0530

    chnages

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -9,6 +9,7 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={() => console.log("clicked")}>Change title</button>
       </div>
     </div>
   );
